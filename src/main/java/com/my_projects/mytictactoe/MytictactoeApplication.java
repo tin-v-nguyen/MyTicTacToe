0e1ffdf84e8c5ebe5e4c0c0d165b1bdabf1e320a
tin-v-nguyen <tinnguyen2002@gmail.com>
2024-11-05 01:33:23 -0500

    Initialize Spring Project

---
+++ src/main/java/com/my_projects/mytictactoe/MytictactoeApplication.java
@@ -0,0 +1,13 @@
+package com.my_projects.mytictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytictactoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytictactoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my_projects/mytictactoe/MytictactoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_projects.mytictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytictactoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
